a8dd665e028b48e1501b1673a7700c5e5806e341
entonoire <43643450+entonoire@users.noreply.github.com>
2021-04-05 13:00:05 +0200

    Update VersionMain.java

--- src/fr/vallfeur/averagecalc/version/VersionMain.java
+++ src/fr/vallfeur/averagecalc/version/VersionMain.java
@@ -20,7 +20,7 @@ import javafx.stage.Stage;
 
 public class VersionMain {
 
-	static Double version = 0.1;
+	static Double version = 0.3;
 	static Double content;
 	static int i = 0;
 	static String versionName;
